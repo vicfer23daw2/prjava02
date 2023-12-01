5a7c7c391861e2251227ff60ed23b7fc24a7a4f1
vicfer23daw2 <442816.clot@fje.edu>
2023-12-01 15:44:26 +0100

    Versió 1.0.1 del projecte prjava02

--- Prjava02.java
+++ Prjava02.java
@@ -19,8 +19,8 @@ public class Prjava02 {
     public static void main(String[] args) {
         System.out.println("versió 0.1 del projecte prjava02"); 	
         try {
-            InetAddress adreça = InetAddress.getLocalHost();
-            String hostname = adreça.getHostName();
+            InetAddress adresa = InetAddress.getLocalHost();
+            String hostname = adresa.getHostName();
             System.out.println("hostname="+hostname);
             System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
